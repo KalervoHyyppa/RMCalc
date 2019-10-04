bd0ace6a47f5339cfa76a4045b241ee08b526960
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2019-10-04 14:07:02 -0600

    Added Js for elements

--- main.js
+++ main.js
@@ -2,3 +2,62 @@ $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip({
     });
 });
+
+function test1() {
+    var x = document.getElementById("mobile");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+function yes() {
+    var x = document.getElementById("PickandPlaceMobile");
+    document.getElementById("dis");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function no() {
+    var x = document.getElementById("PickandPlaceMobile");
+    document.getElementById("dis").remove();
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+// function yes() {
+//     var x = document.getElementById("PickandPlaceMobile");
+//     document.getElementById("PickandPlaceStationary").style.display = "none";
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
+
+// function no() {
+//     var x = document.getElementById("PickandPlaceStationary");
+//     document.getElementById("PickandPlaceMobile").style.display = "none";
+//     if (x.style.display === "block") {
+//         x.style.display = "none";
+//     } else {
+//         x.style.display = "block";
+//     }
+// }
+
+function calc() {
+    var x = document.getElementById("evil");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
